918feaa5b7634c629e641f62dc77457be5f57d79
XYphrodite <72796194+XYphrodite@users.noreply.github.com>
2022-10-14 00:34:38 +0300

    Update main.py
    fix

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import math
 from math import *
 
 # consts
 
 E = 0.01
-dlt = E / 1
+dlt = E / 10
 
 
 def fooA(x1, x2):
@@ -17,79 +18,88 @@ def fooA(x1, x2):
 
 
 def powel():
-    x1 = 8
+    x1 = -10
     x2 = 8
-    print([x1, x2])
-    x1i = x1
-    x2i = x2
 
     while (True):
         y1 = fooA(x1, x2)
         y2 = fooA(x1 - dlt, x2)
-        hF = False
-        vF = False
-        dF = False
         horpoints = 0
         verpoints = 0
         if y1 >= y2:
-            hF = True
             while y1 >= y2:
                 # print("x1 = ", x1, " x2 = ", x2, " f1 = ", y1, " f2 = ", y2)
-                x1 -= dlt
                 horpoints -= 1
                 y1 = fooA(x1, x2)
-                y2 = fooA(x1 - dlt, x2)
+                x1 -= dlt
+                y2 = fooA(x1, x2)
             x1 -= dlt
+            horpoints += 1
         else:
-            hF = True
-            while y1 < y2:
+            while y1 > y2:
                 # print("x1 = ", x1, " x2 = ", x2, " f1 = ", y1, " f2 = ", y2)
-                x1 += dlt
+
                 horpoints += 1
                 y1 = fooA(x1, x2)
-                y2 = fooA(x1 - dlt, x2)
+                x1 += dlt
+                y2 = fooA(x1, x2)
             x1 += dlt
-        print([x1, x2])
+            horpoints -= 1
+        # print([x1, x2])
         y1 = fooA(x1, x2)
         y2 = fooA(x1, x2 - dlt)
         if y1 >= y2:
-            vF = True
             while y1 >= y2:
                 # print("x1 = ", x1, " x2 = ", x2, " f = ", fooA(x1, x2))
-                # print("x1 = ", x1, " x2 = ", x2 - d, " f = ", fooA(x1, x2-d))
+                print("34rtv c")
 
                 verpoints -= 1
                 y1 = fooA(x1, x2)
                 x2 -= dlt
                 y2 = fooA(x1, x2)
-
+            x2 += dlt
+            verpoints += 1
         else:
-            vF = True
-            while y1 < y2:
+            while y1 > y2:
                 # print("x1 = ", x1, " x2 = ", x2 - d, " f = ", fooA(x1, x2 - d))
-
                 verpoints += 1
                 y1 = fooA(x1, x2)
                 x2 += dlt
                 y2 = fooA(x1, x2)
-        print([x1, x2])
+            x2 -= dlt
+            verpoints -= 1
+        # print([x1, x2])
         print(horpoints)
         print(verpoints)
 
-        x1 = x1i
-        x2 = x2i
+        if (math.fabs(horpoints) <= 2) and (math.fabs(verpoints) <= 2):
+            return [x1, x2]
+
         y1 = fooA(x1, x2)
-        y2 = fooA(x1 + dlt * horpoints, x2 + dlt * verpoints)
-        while y1 >= y2:
-            dF = True
-            print("x1 = ", x1, " x2 = ", x2, " f = ", y1, " f2 ",y2 )
-            y1 = fooA(x1, x2)
-            x1 += dlt * horpoints/100
-            x2 += dlt * verpoints/100
-            y2 = fooA(x1, x2)
-        #if (hF == False) and (vF == False) and (dF == False):
-        return [x1, x2]
+        y2 = fooA(x1 + dlt * horpoints / 100, x2 + dlt * verpoints / 100)
+        if y1 > y2:
+            while y1 > y2:
+                print("wnjefd c")
+                y1 = fooA(x1, x2)
+                x1 -= dlt * horpoints / 100
+                x2 -= dlt * verpoints / 100
+                y2 = fooA(x1, x2)
+            x1 += dlt * horpoints / 100
+            x2 += dlt * verpoints / 100
+        elif y1 < y2:
+            while y1 < y2:
+                print("werf")
+                y1 = fooA(x1, x2)
+                x1 += dlt * horpoints / 100
+                x2 += dlt * verpoints / 100
+                y2 = fooA(x1, x2)
+            x1 += dlt * horpoints / 100
+            x2 += dlt * verpoints / 100
+        else:
+            return [x1, x2]
+
+        print([x1, x2])
 
 
 print("Start:")
-print("Powel = ",powel())
+print("Powel = ", powel())
