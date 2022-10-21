e79754ab317fed65292ee8b8f342cd1d696d2a41
Maksim <mrmaksoracle89537089833@yandex.ru>
2022-10-21 11:49:33 +0300

    в

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 import math
 from math import *
+from tkinter import S
 import numpy as np
+import matplotlib.pyplot as plt
 
 E = 0.01
 dlt = E / 100
 
 
+def fooA(x1, x2):
+    a = 6
+    b = -1
+    c = 3
+    d = 4
+    alf = 70
+    return (pow(((x1 - a) * cos(alf) + (x2 - b) * sin(alf)), 2)) / (c * c) + \
+           (pow(((x2 - b) * cos(alf) - (x1 - a) * sin(alf)), 2)) / (d * d)
+
+
+def fooB(x1, x2):
+    return 100 * pow((x2 - pow(x1, 2)), 2) + pow((1 - x1), 2)
+
+
 def foo(x1, x2, n):
     if n == 0:
-        a = 6
-        b = -1
-        c = 3
-        d = 4
-        alf = 70
-        return (pow(((x1 - a) * cos(alf) + (x2 - b) * sin(alf)), 2)) / (c * c) + \
-               (pow(((x2 - b) * cos(alf) - (x1 - a) * sin(alf)), 2)) / (d * d)
+        return fooA(x1, x2)
     else:
-        return 100 * pow((x2 - pow(x1, 2)), 2) + pow((1 - x1), 2)
+        return fooB(x1, x2)
 
 
-def powell(dlt, n):
-    x1 = 0
-    x2 = 0
+def powell(pi, dlt, n):
+    x1 = pi[0]
+    x2 = pi[1]
     while (True):
         y1 = foo(x1, x2, n)
         y2 = foo(x1 + dlt, x2, n)
@@ -106,7 +116,7 @@ def FindGCenter(p, n):
     return xc
 
 
-def symplex(dlt, n):
+def symplex(pi, dlt, n):
     def y(arr):
         return arr[2]
 
@@ -118,9 +128,9 @@ def symplex(dlt, n):
     #####################################
     # задаётся n+1 точка
     #    x1  x2 f
-    p1 = [0, 0, 0]
-    p2 = [1, 0, 0]
-    p3 = [0, 1, 0]
+    p1 = pi
+    p2 = [p1[0] + p1[0] * k, p1[1], 0]
+    p3 = [p1[0], p1[1] + p1[1] * k, 0]
     # вычесляются y-ки
     p1[2] = foo(p1[0], p1[1], n)
     p2[2] = foo(p2[0], p2[1], n)
@@ -128,51 +138,52 @@ def symplex(dlt, n):
     p = [p1, p2, p3]
 
     ff = (p[0][2] + p[1][2] + p[2][2]) / 3
-    sig2 = (pow(p[0][2] - ff, 2) + pow(p[1][2] - ff, 2) + pow(p[2][2] - ff, 2)) / 3
-    # находим h, g, l
-    while (sqrt(sig2) >= dlt):
+    sig = (pow(p[0][2] - ff, 2) + pow(p[1][2] - ff, 2) + pow(p[2][2] - ff, 2)) / 3
+
+    while (sqrt(sig) >= dlt):
         # вычесляются y-ки
         p1[2] = foo(p1[0], p1[1], n)
         p2[2] = foo(p2[0], p2[1], n)
         p3[2] = foo(p3[0], p3[1], n)
+        # находим h, g, l
         p.sort(key=y)
         # определяем центр тяжести всех точек кроме h
         xc = FindGCenter(p, n)
-
+        # xr
         xr = [xc[0] + a * (xc[0] - p[2][0]), xc[1] + a * (xc[1] - p[2][1]), 0]
         xr[2] = foo(xr[0], xr[1], n)
 
-        if (xr[2] < p[2][2]):
+        if (xr[2] < p[2][2]):  # <h
             xe = [xc[0] + g * (xc[0] - p[2][0]), xc[1] + g * (xc[1] - p[2][1]), 0]
             xe[2] = foo(xe[0], xe[1], n)
             # растяжение
-            if (xe[2] < p[2][2]): #f0<fl
+            if (xe[2] < p[2][2]):  # f0<fl
                 p[2] = xe
             # отражение
             else:
                 p[2] = xr
             # отражение
-        elif (xr[2] < p[1][2]):
+        elif (xr[2] < p[1][2]):  # xr<g
             p[2] = xr
-        elif (xr[2] < p[2][2]):
-            xcon = [xc[0] + b * (xc[0] - p[2][0]), xc[1] + b * (xc[1] - p[2][1]), 0]
-            xcon[2] = f(xcon[0], xcon[1])
-            # outside contract
+        elif (xr[2] < p[2][2]):  # xr<h
+            xcon = [xc[0] + b * (xc[0] - p[2][0]), xc[1] + b * (xc[1] - p[2][1]), 0]  # x сжатия
+            xcon[2] = foo(xcon[0], xcon[1], n)
+            # сжатие
             if (xcon[2] <= xr):
                 p[2] = xcon
             # уменьшение
             else:
                 for i in range(1, N + 1):
-                    x[i][0] = (x[i][0] + x[0][0]) / 2
-                    x[i][1] = (x[i][1] + x[0][1]) / 2
-                    x[i][2] = f(x[i][0], x[i][1])
+                    p[i][0] = (p[i][0] + p[0][0]) / 2
+                    p[i][1] = (p[i][1] + p[0][1]) / 2
+                    p[i][2] = foo(p[i][0], p[i][1], n)
         else:
-            xcon = [xc[0] + b * (p[2][0] - xc[0]), xc[1] + b * (p[2][1] - xc[1]), 0]
+            xcon = [xc[0] + b * (p[2][0] - xc[0]), xc[1] + b * (p[2][1] - xc[1]), 0]  # x сжатия
             xcon[2] = foo(xcon[0], xcon[1], n)
-            # inside contract
+            # сжатие
             if (xcon[2] < p[2][2]):
                 p[2] = xcon
-            # shrink
+            # уменьшение
             else:
                 for i in range(1, N + 1):
                     p[i][0] = (p[i][0] + p[0][0]) / 2
@@ -185,10 +196,45 @@ def symplex(dlt, n):
     return [(p[0][0] + p[1][0] + p[2][0]) / 3, (p[0][1] + p[1][1] + p[2][1]) / 3]
 
 
+pia1 = [1, 2, 0]
+pib1 = [-1, 2, 1]
+pia2 = [8, 9, 0]
+pib2 = [-4, 2, 1]
+
+Pa = powell(pia1, dlt, 0)
+Sa = symplex(pia1, dlt, 0)
+Pb = powell(pib1, dlt / 42, 1)
+Sb = symplex(pib1, dlt, 1)
 print("Start:")
-print("Powell a) = ", powell(dlt, 0))
-print("Powell b) = ", powell(dlt / 42, 1))
-print("Symplex a) = ", symplex(dlt, 0))
-print("Symplex b) = ", symplex(dlt / 42, 1))
+print("Powell a)1) = ", powell(pia1, dlt, 0))
+print("Powell b)1) = ", powell(pib1, dlt / 42, 1))
+print("Powell a)2) = ", powell(pia2, dlt, 0))
+print("Powell b)2) = ", powell(pib2, dlt / 42, 1))
+print("Symplex a)1) = ", symplex(pia1, dlt / 4, 0))
+print("Symplex b)1) = ", symplex(pib1, dlt / 42, 1))
+print("Symplex a)2) = ", symplex(pia2, dlt / 4, 0))
+print("Symplex b)2) = ", symplex(pib2, dlt / 42, 1))
 
+fig, (ax1, ax2) = plt.subplots(1, 2)
 
+levels = [-1, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.10, -0.05, -0.01, 0.0, 0.01, 0.05, 0.10, 0.20, 0.30,
+          0.4,
+          0.5, 0.6, 0.7, 0.8, 0.9, 1]
+xa1 = np.arange(2, 10, 0.125)
+xa2 = np.arange(-5, 3, 0.125)
+xa1, xa2 = np.meshgrid(xa1, xa2)
+f2 = np.vectorize(fooA)
+ya = f2(xa1, xa2)
+conta = ax2.contour(xa1, xa2, ya, levels=levels)
+ax2.plot(Pa[0], Pa[1], color="pink", marker=".")
+ax2.plot(Sa[0], Sa[1], color="blue", marker=".")
+
+xb1 = np.arange(-27, 28, 0.125)
+xb2 = np.arange(-24, 26, 0.125)
+xb1, xb2 = np.meshgrid(xb1, xb2)
+f2b = np.vectorize(fooB)
+yb = f2b(xb1, xb2)
+contb = ax1.contour(xb1, xb2, yb, levels=10)
+ax1.plot(Pb[0], Pb[1], color="pink", marker=".")
+ax1.plot(Sb[0], Sb[1], color="blue", marker=".")
+plt.show()
